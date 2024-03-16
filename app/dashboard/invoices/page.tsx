f258a171310c1a0e3ebf0078c39ade66138872f6
ngimdock <ngimdockzemfack@gmail.com>
2024-03-16 12:10:35 +0100

    implement pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 type Props = {
   searchParams?: {
@@ -16,6 +17,8 @@ type Props = {
 export default async function Page({ searchParams }: Props) {
   const query = searchParams?.query ?? '';
   const currentPage = Number(searchParams?.page ?? 1);
+
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -30,7 +33,7 @@ export default async function Page({ searchParams }: Props) {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
